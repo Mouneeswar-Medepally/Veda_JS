bcf8bdef415bbd1226487376c8b2384279b80700
Mouneeswar_Medepally <mouneeswarmedepally@gmail.com>
2024-08-09 10:29:41 +0530

    oops_09_08_2024

--- oops/script.js
+++ oops/script.js
@@ -3,12 +3,12 @@
 //       this.name = name;
 //       this.age = age;
 //     }
-  
+
 //     greet() {
 //       console.log(`Hello, my name is ${this.name}`);
 //     }
 //   }
-  
+
 //   const person1 = new Person('Alice', 30);
 //   console.log(person1.greet)
 //   person1.greet();
@@ -43,29 +43,162 @@
 // } 
 // console.log(myObj.__proto__)
 
-class Animal {
-    constructor(name) {
-      this.name = name;
-    }
-  
-    speak() {
-      console.log(`${this.name} makes a noise`);
-    }
-  }
-  
-  class Dog extends Animal {
-    constructor(name, breed) {
-      super(name);
-      this.breed = breed;
-    }
-    speak() {
-      console.log(`${this.name} barks`);
-    }
-  }
-  
-  const dog = new Dog('Rex', 'German Shepherd');
-  console.log(dog);
+// class Animal {
+//     constructor(name) {
+//       this.name = name;
+//     }
+
+//     speak() {
+//       console.log(`${this.name} makes a noise`);
+//     }
+//   }
+
+//   class Dog extends Animal {
+//     constructor(name, breed) {
+//       super(name);
+//       this.breed = breed;
+//     }
+//     speak() {
+//       console.log(`${this.name} barks`);
+//     }
+//   }
+
+//   const dog = new Dog('Rex', 'German Shepherd');
+//   console.log(dog);
 //   console.log(dog.__proto__)
 //   console.log(dog.speak('dog'))
-  dog.speak(); 
+//   dog.speak(); 
 
+// function main(){
+//     function myFunc(){
+//         console.log(this)
+//     }
+//     const myArrowFunc=()=>{
+//         console.log(this)
+//     }
+//     myFunc()
+//     myArrowFunc()
+// }
+// const obj={
+//     greet:function(){
+//         console.log(this)
+//     }
+// }
+
+// const obj1={
+//     greet:()=>{
+//         console.log(this)
+//     }
+// }
+// obj.greet()
+// obj1.greet()
+
+// Object.keys(obj)
+// class MathUtil {
+//     static square(number) {
+//         return number * number;
+//     }
+// }
+// console.log(MathUtil.square(4));
+
+// function Counter() {
+//     let count = 0;
+  
+//     this.increment = function () {
+//       count += 1;
+//       console.log(count);
+//     };
+  
+//     this.decrement = function () {
+//       count -= 1;
+//       console.log(count);
+//     };
+//   }
+  
+//   const counter = new Counter();
+//   console.log(counter.count)
+//   counter.increment(); // Output: 1
+//   counter.decrement(); // Output: 0
+
+// class Counter {
+//     #count = 0; // Private variable
+  
+//     #log() { // Private method
+//       console.log('Count:', this._count);
+//     }
+  
+    // increment() {
+    //   this.#count += 1;
+    //   this.#log();
+    // }
+  
+    // decrement() {
+    //   this.#count -= 1;
+    //   this.#log();
+    // }
+//   }
+  
+//   const counter = new Counter();
+//   console.log(counter._count)
+//   counter.increment(); // Output: Count: 1
+//   counter.decrement(); // Output: Count: 0
+
+// const obj={
+//    name:'cams'
+// }
+// function greet(...args){
+//     console.log(this)
+// }
+// greet.call(obj,23,'Hyderabad')
+// greet.apply(obj,[23,'Hyderabad'])
+// const boundFunction=greet.bind(obj)
+// boundFunction()
+
+// class Person {
+//     constructor(name, age) {
+//       this.name = name;
+//       this.age = age;
+//     }
+
+//     greet() {
+//       console.log(`Hello, my name is ${this.name}`);
+//     }
+//   }
+
+//   const person1 = new Person('Alice', 30);
+//   console.log(person1.greet)
+//   person1.greet();
+
+
+class Person {
+    constructor(name, age) {
+      this._name = name;
+      this._age = age; // The underscore indicates a "private" field (by convention)
+    }
+  
+    // Getter for name
+    get name() {
+      return this._name;
+    }
+  
+    // Setter for name
+    set name(newName) {
+        this._name = newName;
+    }
+  
+    // Getter for age
+    get age() {
+      return this._age;
+    }
+  
+    // Setter for age
+    set age(newAge) {
+        this._age = newAge;
+    }
+  }
+  
+  const person = new Person('Alice', 25);
+  
+  console.log(person.name); // Getter is called, Output: "Alice"
+  person.name = 'Bob';      // Setter is called, changes _name to "Bob"
+  console.log(person.name); // Getter is called, Output: "Bob"
