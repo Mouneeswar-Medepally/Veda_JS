a9d01e35e2678eded97e5fdb56ca8036682e8ac7
Mouneeswar_Medepally <mouneeswarmedepally@gmail.com>
2024-08-16 12:31:24 +0530

    Class_16_08_2024

---
+++ Class_16_08_2024/script.js
@@ -0,0 +1,47 @@
+// let mySet = new Set([1, 2, 3, 4, 4]);
+// console.log(mySet)
+// console.log(mySet.size)
+// console.log(mySet.add(5))
+// console.log(mySet.add(5))
+// console.log(mySet.delete(4))
+// console.log(mySet.has(3))
+// for (let value of mySet) {
+//     console.log(value);
+// }
+// for(let i=0;i<mySet.size;i++){
+//     console.log(mySet[i])
+// }
+// const obj={name:'cams'}
+// let myMap = new Map([
+//     [true,'yes'],
+//     [obj, 'cams']
+// ]);
+// // console.log(myMap.get(obj))
+// for (let [key, value] of myMap) {
+//     console.log(`${key}: ${value}`);
+// }
+
+// const arr=[1,2,3];
+// arr['name']='cams';
+// console.log(arr)
+
+// for(let elem of arr){
+//     console.log(elem)
+// }
+// for(let elem in arr){
+//     console.log(elem)
+// }
+
+// let obj={
+//     name:'cams'
+// }
+// Object.defineProperty(obj,'age',{
+//     value:23,
+//     enumerable:true,
+//     writable:true
+// })
+// obj.age=30
+// console.log(obj);
+// for(let elem in obj){
+//     console.log(elem)
+// }
